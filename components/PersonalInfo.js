13ad0758ad4fcec16380a763e7ef68a9aca45865
Ilya Konsty <39833967+Ikonsty@users.noreply.github.com>
2023-04-27 17:04:27 +0300

    Personal info and sidebar component

---
+++ components/PersonalInfo.js
@@ -0,0 +1,218 @@
+import React, { useState } from 'react';
+import { Rating, TextField } from '@mui/material';
+import { FaStar, FaRegStar } from 'react-icons/fa';
+import { RiFolderUserLine } from 'react-icons/ri';
+import { MdEdit } from 'react-icons/md';
+import Image from 'next/image';
+
+function PersonalInfo({
+	name,
+	descr,
+	about,
+	specialization,
+	phone,
+	date,
+	email,
+	rate,
+}) {
+	const [showForm, setShowForm] = useState(false);
+	const [emailVal, setEmailVal] = useState(email);
+	const [aboutVal, setAboutVal] = useState(about);
+	const [dateVal, setDateVal] = useState(date);
+	const [specializationVal, setSpecializationVal] = useState(specialization);
+	const [phoneVal, setPhoneVal] = useState(phone);
+
+	const inputStyle = {
+		color: '#8E8F94',
+		borderRadius: '10px',
+	};
+
+	return (
+		<div
+			className='p-3 bg-[#fff] rounded-3xl drop-shadow-lg w-[65vw] h-[45vh]
+         py-7 px-10 flex flex-col justify-between'
+		>
+			<div className='flex flex-row'>
+				<Image
+					className='h-[15vh] w-[8vw] object-cover rounded-[20px]'
+					src='/profile.jpg'
+					width={160}
+					height={160}
+					alt='your photo'
+				/>
+				<div className='ml-7 flex-grow'>
+					<div className='flex flex-row justify-between w-full'>
+						<div className='info'>
+							<div className='text-[23px] font-medium text-primary'>{name}</div>
+							<div className='text-[12px] text-dark-gray'>{descr}</div>
+						</div>
+						{showForm ? (
+							<></>
+						) : (
+							<button
+								className='text-[32px] text-primary items-center'
+								onClick={() => setShowForm((prevCheck) => !prevCheck)}
+							>
+								<MdEdit className='border border-primary p-1 rounded-lg' />
+							</button>
+						)}
+					</div>
+
+					{showForm ? (
+						<form className='h-[220px] flex flex-col py-10'>
+							<div className='flex justify-between'>
+								<div>
+									<div className='text-dark-gray text-[19px] pb-3'>About</div>
+									<div className='text-[14px] w-[15vw] pb-[51px]'>
+										<TextField
+											multiline
+											rows={6}
+											maxRows={7}
+											value={aboutVal}
+											onChange={(e) => setAboutVal(e.target.value)}
+										/>
+									</div>
+								</div>
+								<div>
+									<div>
+										<div className='text-dark-gray text-[19px] pb-3'>
+											Specialization
+										</div>
+										<div className='text-[14px] w-[15vw] pb-[51px]'>
+											<TextField
+												id='outlined-basic'
+												size='small'
+												value={specializationVal}
+												style={inputStyle}
+												onChange={(e) => setSpecializationVal(e.target.value)}
+												variant='outlined'
+											/>
+										</div>
+									</div>
+									<div>
+										<div className='text-dark-gray text-[19px] pb-3'>
+											Phone number
+										</div>
+										<div className='text-[14px] w-[15vw]'>
+											<TextField
+												id='outlined-basic'
+												size='small'
+												value={phoneVal}
+												onChange={(e) => setPhoneVal(e.target.value)}
+												variant='outlined'
+											/>
+										</div>
+									</div>
+								</div>
+								<div>
+									<div>
+										<div className='text-dark-gray text-[19px] pb-3'>
+											Date Of Birth
+										</div>
+										<div className='text-[14px] w-[15vw] pb-[51px]'>
+											<TextField
+												id='outlined-basic'
+												size='small'
+												value={dateVal}
+												onChange={(e) => setDateVal(e.target.value)}
+												variant='outlined'
+											/>
+										</div>
+									</div>
+									<div>
+										<div className='text-dark-gray text-[19px] pb-3'>Email</div>
+										<div className='text-[14px] w-[15vw] pb-[45px]'>
+											<TextField
+												id='outlined-basic'
+												size='small'
+												value={emailVal}
+												onChange={(e) => setEmailVal(e.target.value)}
+												variant='outlined'
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className='flex justify-end'>
+								<button
+									type='submit'
+									onClick={() => setShowForm((prevCheck) => !prevCheck)}
+									className='bg-primary hover:bg-primary-dark text-white font-bold py-4 px-[70px] rounded-lg'
+								>
+									Submit
+								</button>
+							</div>
+						</form>
+					) : (
+						<div className='h-[220px] flex justify-between py-10'>
+							<div>
+								<div className='text-dark-gray text-[19px] pb-3'>About</div>
+								<div className='text-[14px] w-[15vw] pb-[70px]'>{aboutVal}</div>
+							</div>
+							<div>
+								<div>
+									<div className='text-dark-gray text-[19px] pb-3'>
+										Specialization
+									</div>
+									<div className='text-[14px] w-[15vw] pb-[70px]'>
+										{specializationVal}
+									</div>
+								</div>
+								<div>
+									<div className='text-dark-gray text-[19px] pb-3'>
+										Phone number
+									</div>
+									<div className='text-[14px] w-[15vw] pb-[70px]'>
+										{phoneVal}
+									</div>
+								</div>
+							</div>
+							<div>
+								<div>
+									<div className='text-dark-gray text-[19px] pb-3'>
+										Date Of Birth
+									</div>
+									<div className='text-[14px] w-[15vw] pb-[70px]'>
+										{dateVal}
+									</div>
+								</div>
+								<div>
+									<div className='text-dark-gray text-[19px] pb-3'>Email</div>
+									<div className='text-[14px] w-[15vw] pb-[70px]'>
+										{emailVal}
+									</div>
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+
+			{showForm ? (
+				<> </>
+			) : (
+				<div className='flex flex-row justify-between'>
+					<div className='stars'>
+						<div className='text-dark-gray text-[19px]'>Rate</div>
+						<div>
+							<Rating
+								defaultValue={rate}
+								readOnly
+								icon={<FaStar fontSize='20px' />}
+								emptyIcon={<FaRegStar fontSize='20px' />}
+							/>
+						</div>
+					</div>
+					<div className='flex flex-row items-center'>
+						<div className='text-[22px] text-primary pr-[5px]'>
+							<RiFolderUserLine />
+						</div>
+						<div className='text-primary'>CURRENT PATIENTS</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default PersonalInfo;
---
+++ components/Sidebar.js
@@ -0,0 +1,162 @@
+import Image from 'next/image';
+import { VscLink } from 'react-icons/vsc';
+import { BiUser, BiMenu, BiLogOutCircle } from 'react-icons/bi';
+import { BsFillPersonVcardFill } from 'react-icons/bs';
+import { AiFillSchedule } from 'react-icons/ai';
+import { FaUsers } from 'react-icons/fa';
+
+import { useState } from 'react';
+
+function Sidebar({ onToggle }) {
+	const [active, setActive] = useState(false);
+
+	const handleToggle = () => {
+		setActive((active) => {
+			onToggle(!active);
+			return !active;
+		});
+	};
+
+	return (
+		<div
+			className={`sidebar fixed top-0 left-0 h-full bg-primary-dark px-[14px] py-[6px]
+		transition-all duration-500 ease-in-out ${active ? 'w-[240px]' : 'w-[78px]'}`}
+		>
+			<div className='logo_content'>
+				<div
+					className={`flex items-center h-[50px]
+							pointer-events-none transition-all duration-500 ease-in-out 
+							text-white ${active ? 'opacity-100' : 'opacity-0'}`}
+				>
+					<VscLink className='text-[28px] mr-[5px]' />
+					<div className='text-[20px] font-normal'>Leekar</div>
+				</div>
+			</div>
+			<BiMenu
+				className={`absolute text-white top-[22px] text-[20px] h-[19px] w-[50px]
+				text-center leading-[50px] -translate-x-[50%]
+				${active ? 'left-[90%]' : 'left-[50%]'}`}
+				onClick={handleToggle}
+			/>
+
+			<ul className='mt-[20px]'>
+				<li className='group relative h-[50px] w-full my-0 list-none leading-[50px]'>
+					<a className='sidebar-link' href='#'>
+						<BiUser className='icon' />
+						<span
+							className={`transition-all duration-500 ease-in-out
+						${active ? 'opacity-1 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+						>
+							Profile
+						</span>
+					</a>
+					<span
+						className={`tooltip
+						group-hover:transition-all group-hover:duration-500 group-hover:ease-in-out
+						group-hover:opacity-100 group-hover:top-[50%]
+						${active ? 'hidden' : 'block'}`}
+					>
+						Profile
+					</span>
+				</li>
+
+				<li className='group relative h-[50px] w-full my-0 list-none leading-[50px]'>
+					<a className='sidebar-link' href='#'>
+						<AiFillSchedule className='icon' />
+						<span
+							className={`transition-all duration-500 ease-in-out
+						${active ? 'opacity-1 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+						>
+							Schedule
+						</span>
+					</a>
+					<span
+						className={`tooltip
+						group-hover:transition-all group-hover:duration-500 group-hover:ease-in-out
+						group-hover:opacity-100 group-hover:top-[50%]
+						${active ? 'hidden' : 'block'}`}
+					>
+						Schedule
+					</span>
+				</li>
+
+				<li className='group relative h-[50px] w-full my-0 list-none leading-[50px]'>
+					<a className='sidebar-link' href='#'>
+						<BsFillPersonVcardFill className='icon' />
+						<span
+							className={`transition-all duration-500 ease-in-out
+						${active ? 'opacity-1 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+						>
+							Health card
+						</span>
+					</a>
+					<span
+						className={`tooltip
+						group-hover:transition-all group-hover:duration-500 group-hover:ease-in-out
+						group-hover:opacity-100 group-hover:top-[50%]
+						${active ? 'hidden' : 'block'}`}
+					>
+						Health card
+					</span>
+				</li>
+
+				<li className='group relative h-[50px] w-full my-0 list-none leading-[50px]'>
+					<a className='sidebar-link' href='#'>
+						<FaUsers className='icon' />
+						<span
+							className={`transition-all duration-500 ease-in-out
+						${active ? 'opacity-1 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+						>
+							Patients
+						</span>
+					</a>
+					<span
+						className={`tooltip
+						group-hover:transition-all group-hover:duration-500 group-hover:ease-in-out
+						group-hover:opacity-100 group-hover:top-[50%]
+						${active ? 'hidden' : 'block'}`}
+					>
+						Patients
+					</span>
+				</li>
+			</ul>
+
+			<div className='absolute text-white bottom-0 left-0 w-full'>
+				<div
+					className={`relative px-[10px] py-[6px] h-[60px] bg-none
+				transition-all duration-400 ease-in-out
+				${active ? 'bg-primary' : ''}`}
+				>
+					<div
+						className={` flex items-center whitespace-nowrap
+						transition-all duration-500 ease-in-out
+						${active ? 'opacity-1 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+					>
+						<Image
+							className='h-[45px] w-[45px] object-cover rounded-xl'
+							src='/profile.jpg'
+							width={50}
+							height={50}
+							alt=''
+						/>
+						<div className='ml-[10px]'>
+							<div className='text-[15px] font-normal'>Ilya Konsty</div>
+							<div className='text-[12px]'>Doctor</div>
+						</div>
+					</div>
+					<div
+						className={` log-out-div
+					${active ? 'left-[88%] bg-none' : 'left-[50%] bg-primary'}`}
+					>
+						<BiLogOutCircle
+							className='icon text-[20px] text-white
+						transition-all duration-500 ease-in-out'
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Sidebar;
