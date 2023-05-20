8fdc14d11e4c851fcdcb054548a805f8aa2e33e2
Vladyslav Protsenko <76886116+Vl-tb@users.noreply.github.com>
2023-05-20 21:14:44 +0300

    Delete medcard_microservice directory

--- medcard_microservice/domain/AppointmentNotes.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class AppointmentNotes(BaseModel):
-    patient_id: str
-    doctor_id: str
-    appointment_id: str
-    diagnosis: str
--- medcard_microservice/domain/MedCardInfo.py
+++
@@ -1,8 +0,0 @@
-class MedCardInfo:
-    def __init__(self, name, email, phone, birthdate, apps):
-        self.name_surname = name
-        self.email = email
-        self.phone = phone
-        self.birthdate = birthdate
-        self.appointments = apps
-
